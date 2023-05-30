13655f3386d75a755021e5481024f47b08a81ef9
Sourav <souravbadhan909@gmail.com>
2023-05-30 16:54:29 -0400

    Added the Get and Post to the server and gave shotened the urls,added delete button, edit button,cookies &logout button

--- express_server.js
+++ express_server.js
@@ -62,12 +62,18 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
-
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] }
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -95,6 +101,11 @@ app.post("/urls/:id/edit", (req, res) => {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
